30f913c3897efeb2d7680b3c8398d7ef5e29a2b3
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-09-07 13:18:45 -0500

    done

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -7,11 +7,12 @@ function ArtistView() {
     const [ artistData, setArtistData ] = useState([])
     
 	useEffect(() => {
-        const API_URL = `http://localhost:3000/album/${id}`
+        const API_URL = `http://localhost:4000/album/${id}`
         const fetchData = async () => {
             const response = await fetch(API_URL)
             const resData = await response.json()
             setArtistData(resData.results)
+            
         }
         fetchData()
 	}, [id])
