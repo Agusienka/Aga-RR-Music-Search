34de3a3ae98382c46ff224286af389650fb04854
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-09-07 14:40:31 -0500

    helper file

--- src/App.js
+++ src/App.js
@@ -5,29 +5,22 @@ import SearchBar from './components/SearchBar'
 import AlbumView from './components/AlbumView'
 import ArtistView from './components/ArtistView'
 import { Fragment } from 'react/cjs/react.production.min'
+import { createResource as fetchData } from './helper'
+
 
 function App() {
 	let [search, setSearch] = useState('')
 	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+	let [data, setData] = useState(null)
 
 	const API_URL = 'https://itunes.apple.com/search?term='
 
 	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					return setData(resData.results)
-				} else {
-					return setMessage('Not Found')
-				}
-			}
-			fetchData()
+		if (searchTerm) {
+			setData(fetchData(searchTerm))
 		}
-	}, [search])
+	}, [searchTerm])
+	
 	
 	const handleSearch = (e, term) => {
 		e.preventDefault()
